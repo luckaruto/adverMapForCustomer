833be01270b562847c90c59cd0c3d98cecf7dc23
tranhuanhcmus <tranhuanbs02@gmail.com>
2023-12-23 01:45:16 +0700

    fix BE

--- CK/src/main/java/com/test/CK/Surface/SurfaceService.java
+++ CK/src/main/java/com/test/CK/Surface/SurfaceService.java
@@ -12,38 +12,10 @@ import java.util.Set;
 public class SurfaceService {
     private final  SurfaceRepository surfaceRepository;
     private final SpaceRepository spaceRepository;
-    private final SpaceSurfaceRepository spaceSurfaceRepository;
 
-    public SurfaceService(SurfaceRepository surfaceRepository, SpaceRepository spaceRepository, SpaceSurfaceRepository spaceSurfaceRepository) {
+    public SurfaceService(SurfaceRepository surfaceRepository, SpaceRepository spaceRepository) {
         this.surfaceRepository = surfaceRepository;
         this.spaceRepository = spaceRepository;
-        this.spaceSurfaceRepository = spaceSurfaceRepository;
-    }
-    HttpStatus addToSpace(Short spaceId,Short surfaceId){
-        Optional<Space> spaceOptional=spaceRepository.findById(spaceId);
-        Optional<Surface> surfaceOptional=surfaceRepository.findById(surfaceId);
-
-
-        if(spaceOptional.isEmpty() || surfaceOptional.isEmpty()){
-            return HttpStatus.NOT_FOUND;
-        }else {
-            SpaceSurface spaceSurface=new SpaceSurface();
-            spaceSurface.setSurfaces(surfaceOptional.get());
-            spaceSurface.setSpace(spaceOptional.get());
-            spaceSurfaceRepository.save(spaceSurface);
-            return HttpStatus.OK;
-        }
-    }
-
-    HttpStatus deleteFromSpace(Short spaceId,Short surfaceId){
-        Optional<Space> spaceOptional=spaceRepository.findById(spaceId);
-        Optional<Surface> surfaceOptional=surfaceRepository.findById(surfaceId);
-        if(spaceOptional.isEmpty() || surfaceOptional.isEmpty()){
-            return HttpStatus.NOT_FOUND;
-        }else {
-            spaceSurfaceRepository.deleteBySpaceIdAndSurfacesId(spaceId,surfaceId);
-            return HttpStatus.OK;
-        }
     }
     Set<Surface> getAllBySpaceId(Short id){
         Optional<Space> spaceOptional=spaceRepository.findById(id);
